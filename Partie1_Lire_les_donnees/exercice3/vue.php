d37f2ba3735d0ae7e730870eb470c0c42e904226
Ines Bakhtaoui <inesbkht@gmail.com>
2021-07-27 13:04:40 +0200

    correction + bootstrap

---
+++ Partie1_Lire_les_donnees/exercice3/vue.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>PDO-Exercice 2</title>
+</head>
+
+<body>
+    <h1>Les types de spectacle</h1>
+    <?php
+    // On affiche chaque entrée une à une
+    while ($donnees = $req->fetch()) {
+    ?>
+        <p><?= $donnees['genre'] ?></p>
+    <?php
+    }
+    $req->closeCursor(); // On libère la connexion au serveur, permettant ainsi à d'autres requêtes SQL d'être exécutées, mais on laisse la requête dans un état lui permettant d'être de nouveau exécutée.
+    ?>
+</body>
+
+</html>
